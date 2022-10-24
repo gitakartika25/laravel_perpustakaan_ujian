2b1f56d7ff82e655625584d335f2d344e188a042
gitakartika25 <teresagita25@gmail.com>
2022-10-24 11:50:01 +0700

    add routes and controller

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,14 @@
 <?php
 
+use App\Http\Controllers\BukuController;
+use App\Http\Controllers\DetailPeminjamanController;
+use App\Http\Controllers\DetailPengembalianController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\KelasController;
+use App\Http\Controllers\PeminjamanController;
+use App\Http\Controllers\PengembalianController;
+use App\Http\Controllers\PetugasController;
+use App\Http\Controllers\SiswaController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
@@ -19,7 +27,15 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('dashboard', HomeController::class);
+// Route::resource('dashboard', HomeController::class);
+Route::resource('kelas', KelasController::class);
+Route::resource('siswa', SiswaController::class);
+Route::resource('buku', BukuController::class);
+Route::resource('petugas', PetugasController::class);
+Route::resource('peminjaman', PeminjamanController::class);
+Route::resource('pengembalian', PengembalianController::class);
+Route::resource('detailpeminjaman', DetailPeminjamanController::class);
+Route::resource('detailpengembalian', DetailPengembalianController::class);
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/dashboard', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
